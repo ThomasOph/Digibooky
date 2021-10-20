d8e94f1f318dcb0818189b7d2cb8d5d6b57c75a0
Hans Reygaert <HansRDev@outlook.com>
2021-10-20 11:34:08 +0200

    inital commit

---
+++ src/main/java/com/switchfully/ctrlaltdefeatdigibooky/DigibookyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.ctrlaltdefeatdigibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigibookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigibookyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/switchfully/ctrlaltdefeatdigibooky/DigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.ctrlaltdefeatdigibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibookyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
