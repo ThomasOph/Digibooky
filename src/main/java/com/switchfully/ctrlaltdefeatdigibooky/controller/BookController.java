332b6deb918c359b6b46420e04e37d45262a00af
Christoph Parrez <christoph.parrez@gmail.com>
2021-10-21 11:18:40 +0200

    Spring Boot Starter Web Dependency

--- src/main/java/com/switchfully/ctrlaltdefeatdigibooky/controller/BookController.java
+++ src/main/java/com/switchfully/ctrlaltdefeatdigibooky/controller/BookController.java
@@ -1,6 +1,10 @@
 package com.switchfully.ctrlaltdefeatdigibooky.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "books")
 public class BookController {
 
 }
