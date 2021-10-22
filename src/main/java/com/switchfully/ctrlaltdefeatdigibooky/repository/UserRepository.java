4def22ac9e4240d7fd918982a126f432051f8ee7
Hans Reygaert <HansRDev@outlook.com>
2021-10-22 13:43:01 +0200

    Added librarian

--- src/main/java/com/switchfully/ctrlaltdefeatdigibooky/repository/UserRepository.java
+++ src/main/java/com/switchfully/ctrlaltdefeatdigibooky/repository/UserRepository.java
@@ -16,7 +16,9 @@ public class UserRepository {
       userRepository = new HashMap<>();
       userRepository.put("TestID", new User(UserRole.MEMBER, "TestID", "Mail@mail.com", "Thomas", "Opheide", "Blah", "20", "9000", "Gent"));
       userRepository.put("TheAdmin", new User(UserRole.ADMIN, "TheAdmin", "Admin@mail.com", "Wouter", "Www", "Blah", "20", "9000", "Gent"));
-
+      userRepository.put("TheLibrarian", new User(UserRole.LIBRARIAN,
+              "TheLibrarian", "librarian@gmail.com", "Hans", "Reygaert",
+              "Blah", "22", "9160", "Lokeren"));
    }
 
    public Map<String, User> getUserRepository() {
